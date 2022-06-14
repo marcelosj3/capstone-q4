42d3e6ac2a53d6e3b8dd3d70885d329c037f16b4
Leandro Medvedev <leandromedvedev@hotmail.com>
2022-06-14 17:59:31 -0300

    feat: validate token structure folder

---
+++ src/middlewares/validateToken.middleware.ts
@@ -0,0 +1,9 @@
+// import { NextFunction, Request, Response } from 'express';
+
+// export const validateTokenMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+//     const token: string = req.headers.authorization?.split(" ")[1];
+
+//     if () {
+        
+//     }
+// }
