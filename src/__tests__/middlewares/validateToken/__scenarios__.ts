e09d6e155f4adffa377b543b44341b96238b5f43
Marcelo Silveira <marcelosj3@hotmail.com>
2022-06-15 17:53:58 -0300

    feat: validate token tests and jwt mocks
    Created the validate token tests;
    Created the mocks and implementations to fulfill the needs of the test.

--- src/__tests__/middlewares/validateToken/__scenarios__.ts
+++ src/__tests__/middlewares/validateToken/__scenarios__.ts
@@ -1,3 +1,50 @@
-const validateToken = {
-  payload: {},
+import { NextFunction, Request, Response } from 'express';
+
+export const validateTokenWithMissingToken = {
+  payload: {
+    req: { headers: { authorization: '' } } as Request,
+    res: {} as Response,
+    next: {} as NextFunction,
+  },
+  expected: {
+    status: 401,
+    message: { error: 'Missing authorization token' },
+  },
+};
+
+export const validateTokenWithMalformedJwt = {
+  payload: {
+    req: { headers: { authorization: 'Bearer invalid' } } as Request,
+    res: {} as Response,
+    next: {} as NextFunction,
+  },
+  expected: {
+    status: 401,
+    message: { error: 'jwt malformed' },
+  },
+};
+
+export const validateTokenWithInvalidToken = {
+  payload: {
+    req: { headers: { authorization: 'Bearer invalid.auth.token' } } as Request,
+    res: {} as Response,
+    next: {} as NextFunction,
+  },
+  expected: {
+    status: 401,
+    message: { error: 'invalid token' },
+  },
+};
+
+export const validateTokenSuccessfully = {
+  payload: {
+    req: {
+      headers: {
+        authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNiZjRjZTFlLWM4YzQtNDU3NC1hNmU4LTI0ZGFhZjlmOTkyZCIsImlhdCI6MTY1NTMyNjIwNiwiZXhwIjoxNjU1NDEyNjA2fQ.GTrwScTY9FN1H1wR_-MtikM7cBo61jAoK9zNlm2zhOE',
+      },
+    } as Request,
+    res: {} as Response,
+    next: jest.fn() as NextFunction,
+  },
 };
