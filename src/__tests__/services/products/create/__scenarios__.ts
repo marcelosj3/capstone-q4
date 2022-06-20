be3d8b8ff1a5c52cfd5541a4c17b5a623d4d49a2
Gustavo Ribeiro Sergio <ribeiro.gustavo95@gmail.com>
2022-06-19 23:25:53 -0300

    created folder

---
+++ src/__tests__/services/products/create/__scenarios__.ts
@@ -0,0 +1,11 @@
+export const createProduct = {
+  products: {},
+  payload: {
+    name: 'Leite 1l',
+    brand: 'Terra Viva',
+    category: 'Lactiecinios',
+    description: 'Leite terra-viva 1l',
+    expiryDate: '25/08/2030',
+  },
+  expected: {},
+};
